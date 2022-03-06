bef1d8e8329283b65ce0ac4de880fe23fe743952
Nestyles <tom.sancho@epitech.eu>
2022-03-06 15:37:11 +0100

    [ADD] my_strcat and updated my_strlen prototype

--- include/my_strings.h
+++ include/my_strings.h
@@ -8,11 +8,21 @@
 #ifndef __MY_STRINGS_H__
     #define __MY_STRINGS_H__
 
+typedef unsigned long size_t;
+
 /**
 ** @brief Get the size of a string
 ** @param str null-terminated string
 ** @return Size of str
 **/
-int my_strlen(char const *str);
+size_t my_strlen(char const *str);
+
+/**
+** @brief Concatenate 2 strings
+** @param dest buffer which will hold the result
+** @param src string concatened into dest
+** @return pointer to dest
+*/
+char *my_strcat(char *dest, char const *src);
 
 #endif
