30d29a656de8e55bf45582ca07484fc547bcdeab
Nestyles <tom.sancho@epitech.eu>
2022-03-06 13:18:55 +0100

    [ADD] my_strings.h which will contain all public functions

---
+++ include/my_strings.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** include/my_strings
+** File description:
+** my_strings library include :)
+*/
+
+#ifndef __MY_STRINGS_H__
+    #define __MY_STRINGS_H__
+
+/**
+** @brief Get the size of a string
+** @param str null-terminated string
+** @return Size of str
+**/
+int my_strlen(char const *str);
+
+#endif
